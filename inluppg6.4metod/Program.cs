83d95c89ea9d023f4b36a35a787758d14ec1450f
Admin <Admin@TEB12707>
2023-02-10 11:10:33 +0100

    Add project files.

---
+++ inluppg6.4metod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace inluppg64metod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Tal(Math.Pow(5.6,4), 5 ^ 4))
+        }
+        static double Tal(double tal1,double tal2)
+        {
+
+        }
+    }
+}
